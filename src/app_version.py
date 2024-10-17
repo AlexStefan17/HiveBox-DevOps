2dcec1630b2341ea61c8c4ca5d9009a23fdee7af
Alex Stefan <stefan.alex170299@gmail.com>
2024-10-17 23:06:06 +0300

    feat: add sys library

--- src/app_version.py
+++ src/app_version.py
@@ -1,7 +1,11 @@
+"""Module providing a function printing python version."""
+import sys
+
 def print_version():
+    """Function printing project version."""
     version = "v0.0.1"
     print(f"Current app version: {version}")
+    sys.exit(0)
 
-# Example usage
 if __name__ == "__main__":
     print_version()
