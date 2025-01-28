7422f54b54eeb544490597f4b82c5be6f07264e3
Alex Stefan <stefan.alex170299@gmail.com>
2025-01-28 22:36:06 +0200

    chore: update version to v0.3.0 and updated app_version.py

--- src/app_version.py
+++ src/app_version.py
@@ -1,11 +1,25 @@
 """Module providing a function printing python version."""
 import sys
+import os
 
-VERSION = "v0.2.0"
+def get_version_from_file(file_path="version.txt"):
+    """Reads the version from the specified version file using a relative path."""
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    file_path = os.path.join(current_dir, file_path)
+    
+    if not os.path.isfile(file_path):
+        return "Version file not found"
+    
+    with open(file_path, "r") as file:
+        version = file.read().strip()
+    return version
+
 
 def print_version():
-    """Function printing project version."""
-    print(f"Current app version: {VERSION}")
+    """Function printing the project version."""
+    version = get_version_from_file()
+    print(f"Current app version: {version}")
     sys.exit(0)
 
 if __name__ == "__main__":
